a01795a1086c2ae0566d5984651ce88393428360
gulnazsiddiqui <gulnazgs7896@gmail.com>
2024-08-30 18:16:43 +0530

    Uploaded file

---
+++ date.js
@@ -0,0 +1,13 @@
+
+module.exports.getData =()=>
+{
+    let today=new Date()
+let options ={
+    weekday:'long',
+    month:'long',
+    day:'numeric'
+
+}
+
+return today.toLocaleDateString('en-US',options)
+}
